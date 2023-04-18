9db8bd5a8d484c93fb48409d52fbf44911277634
EloiDomingos <eloi.domingos@hotmail.com>
2023-04-18 17:45:18 -0300

    Inserindo tipos de variáveis como exemplo

--- tipos-variavies/src/TiposVariaveis.java
+++ tipos-variavies/src/TiposVariaveis.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class TiposVariaveis {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+           double salarioMinimo = 2500; //forma correta de atribuir 2 mil e quinhentos
+           double preco = 1500.50;
+           System.out.println(salarioMinimo);
+           System.out.println(preco);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+           int numero = 5;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+           numero = 8; //atribuindo um novo valor a variavel "numero"
+
+           final String BR = "Brasil"; // definindo uma constante através da palavra "final"
+
         }
     }
-}
