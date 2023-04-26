79d829c28239a970828096755fb8865afd271135
EloiDomingos <eloi.domingos@hotmail.com>
2023-04-26 13:58:53 -0300

    lista encadeada

---
+++ Estrutura de Dados/listaEncadeada/src/com/listaEncadeada/ListaEncadeada.java
@@ -0,0 +1,94 @@
+package com.listaEncadeada;
+
+public class ListaEncadeada<T> {
+    No<T> referenciaEntrada;
+
+    public ListaEncadeada(){
+        this.referenciaEntrada = null;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i =0; i < this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        noAuxiliar.setProximoNo(novoNo);
+
+    }
+
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for(int i =0; i <= index; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    public T remove(int index){
+        No<T> noPivor = this.getNo(index);
+        if(index == 0){
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+
+        No<T> noAnterior = getNo(index -1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        return noPivor.getConteudo();
+    }
+
+    private void validaIndice(int index){
+        if(index >= size()){
+            int ultimoIndice = size()-1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo no indice " + index + " desta lista. Esta lista vai até " + ultimoIndice + ".");
+        }
+    }
+
+    public int size(){
+        int tamanholista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true){
+            if(referenciaAux != null){
+                tamanholista++;
+                if(referenciaAux.getProximoNo() != null){
+                    referenciaAux = referenciaAux.getProximoNo();
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        return tamanholista;
+    }
+
+    public boolean isEmpty(){
+        return referenciaEntrada == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRertorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i = 0; i < this.size(); i++){
+            strRertorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRertorno += "null";
+
+        return strRertorno;
+    }
+}
